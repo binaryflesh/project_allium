24552c08436ff575ae7d577d26d99a24ab95f0bf
arosso3 <rossoklakovich@gmail.com>
2018-10-25 15:16:54 -0500

    Added Commit file selection

--- commit and push changes to fork.sh
+++ commit and push changes to fork.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+git add -p
+
 echo "Type the summary you want for your commit and press enter"
 
 read summary
 
-git add -A && git commit -m $summary
+git commit -m "$summary"
 
 git push origin master
 read -p "Press Return to Close..."
