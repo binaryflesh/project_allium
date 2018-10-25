033163c02f4a9fdcc9172dc76c43b22b7f928c8b
arosso3 <rossoklakovich@gmail.com>
2018-10-25 17:05:00 -0500

    Added comments and ability to just oush to push/comit cile[D[D[D[D[D[D[Dm[C[C[C[C[script

--- commit and push changes to fork.sh
+++ commit and push changes to fork.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-git add -p
 
-echo "Type the summary you want for your commit and press enter"
+# Asks user if they want to commit files before pushing
+read -p "Do you want to commit (y/n)?:" commit_statement
 
-read summary
+# If user wants to commit files, commit files
+if [ "$commit_statement" == "y" ]
+then
+    # Displays file changes and prompts user on whether or not they want to commith them
+    git add -p
 
-git commit -m "$summary"
+    # Display Message asking for Commit summary
+    echo "Type the summary you want for your commit and press enter"
 
+    # Prompt for user input and save to varriable summary
+    read summary
+
+    # Commits changes to repository
+    git commit -m "$summary"
+fi
+#Pushes changes to remote repository
 git push origin master
+
+# Prompts for user input when done 
+# (this is so that the console stays open so that user can read any error messages)
 read -p "Press Return to Close..."
