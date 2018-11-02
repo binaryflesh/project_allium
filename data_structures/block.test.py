9642ca3b3abd3d2e427221bcc4df3168b19ae980
jldistefano <43453042+jldistefano@users.noreply.github.com>
2018-11-02 18:27:07 -0500

    Update block.test.py

--- data_structures/block.test.py
+++ data_structures/block.test.py
@@ -192,10 +192,10 @@ class TestBlock(unittest.TestCase):
         data = hash_SHA("0".encode())
         self.assertEqual(32, len(data))
         #Creates a block header by mining with these strings and a target of 10^200
-        bytestring = mine(prev_hash, data, 10**70)
+        bytestring = mine(prev_hash, data, 10**77)
         self.assertEqual(74, len(bytestring))   #NOTE: This fails on Linux Mint
         #Tests if result block header is less than target
-        self.assertTrue(less_than_target(hash_SHA(bytestring)), 10**70)
+        self.assertTrue(less_than_target(hash_SHA(bytestring)), 10**77)
         
     # Generates a block based on an incredibly large target, so the nonce will be zero
     # Compares the result of splice_nonce converted to an integer to zero
