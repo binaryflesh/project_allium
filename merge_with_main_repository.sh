876142fbe51e63489a44064b74754c4b8ae4a5c9
arosso3 <rossoklakovich@gmail.com>
2018-11-07 16:54:23 -0600

    Updated scripts to match new dev process

--- merge_with_main_repository.sh
+++ merge_with_main_repository.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#Fetches any changes that have been made to upstream repository
-git pull upstream master
+# Pulls any changes that have been made to dev branch
+git pull dev
 
 # Push changes to remote repo
-git push origin master
+git push
 
 # Prompts for user input when done 
 # (this is so that the console stays open so that user can read any error messages)
