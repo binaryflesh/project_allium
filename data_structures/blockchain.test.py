560f76103728cba2a674fdc20c9134dcd743b817
jldistefano <jldstefano@gmail.com>
2018-11-09 17:35:00 -0600

    Added setUp() tearDown() to blockchain.test.py

--- data_structures/blockchain.test.py
+++ data_structures/blockchain.test.py
@@ -4,28 +4,25 @@ import os
 from blockchain import *
 
 class TestBlock(unittest.TestCase):
+	def setUp(self):
+		self.bc = Blockchain("testfile")
 
+	def tearDown(self):
+		os.remove("testfile")
 
-    def test_constructor(self):
-        #file that is going into the blockchain
-        filename = "testfile"
-        #creating an object of the Blockchain class
-        test_blockchain = Blockchain(filename)
-        self.assertEqual(test_blockchain.blockfile,filename)
-        self.assertTrue(os.path.isfile(filename))
-        # Opens the blockchain file and writes '0' to it
-        with open(test_blockchain.blockfile, 'wb') as fileobj:
-            fileobj.write(b'0')
-
-        # Creates another blockchain with the same filename as test_blockchain
-        test_blockchain_2 = Blockchain(test_blockchain.blockfile)
-        # Ensures that the file of test_blockchain is not overwritten
-        with open(test_blockchain.blockfile, 'rb') as fileobj:
-            char = fileobj.read(1)
-        self.assertEqual(b'0', char)
-        # Deletes the test file
-        os.remove(filename)
-        self.assertFalse(os.path.isfile(filename))
+	def test_constructor(self):
+		filename = "testfile"
+		self.assertEqual(self.bc.blockfile,filename)
+		self.assertTrue(os.path.isfile(filename))
+		# Opens the blockchain file and writes '0' to it
+		with open(self.bc.blockfile, 'wb') as fileobj:
+		    fileobj.write(b'0')
+		# Creates another blockchain with the same filename as test_blockchain
+		test_blockchain_2 = Blockchain(self.bc.blockfile)
+		# Ensures that the file of test_blockchain is not overwritten
+		with open(self.bc.blockfile, 'rb') as fileobj:
+		    char = fileobj.read(1)
+		self.assertEqual(b'0', char)
 
 if __name__ == '__main__':
     unittest.main()
