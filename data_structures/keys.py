2a0a76df2c33b6065bc99b0fde5435b0a4960aaf
ramirez915 <jose.angelramirez915@gmail.com>
2018-11-10 12:25:47 -0600

    Added private key and tests

--- data_structures/keys.py
+++ data_structures/keys.py
@@ -1,3 +1,12 @@
 import os, ecdsa, hashlib
 
 from block import hashSHA
+
+def generate_private_key():
+    """
+    Creates a random 32 byte string used as a private key
+
+    :no param:
+    :return:    A randomly generated 32 byte string using urandom()
+    """
+    return os.urandom(32)
