3096a7519f9b9c6f4e68e00f3ebe4a3f177722d3
jjex05 <40678761+jjexie05@users.noreply.github.com>
2018-11-11 15:58:29 -0600

    New Function: generate_public_key

--- data_structures/keys.py
+++ data_structures/keys.py
@@ -1,6 +1,7 @@
 import os, ecdsa, hashlib
 
-from block import hashSHA
+from block import hashSHA, hash_SHA
+
 
 def generate_private_key():
     """
@@ -10,3 +11,17 @@ def generate_private_key():
     :return:    A randomly generated 32 byte string using urandom()
     """
     return os.urandom(32)
+
+def generate_public_key(private_key):
+    # Takes private key and puts it through the ellipical curve for public key encryption
+    signing_key = ecdsa.SigningKey.from_string(private_key, curve = ecdsa.SECP256k1)
+    # Return verifying key, which effectively is the public key
+    verifying_key = signing_key.get_verifying_key()
+
+    return verifying_key.to_string()
+
+
+
+
+
+ 
--- data_structures/keys.test.py
+++ data_structures/keys.test.py
@@ -14,6 +14,20 @@ class Test(unittest.TestCase):
         self.assertEqual(32,len(key))
         self.assertIsInstance(key, bytes)
 
+    def test_generate_public_key(self):
+        key = generate_private_key()
+        public_key = generate_public_key(key)
+        self.assertIsInstance(public_key, bytes)
+        signing_key = ecdsa.SigningKey.from_string(key, curve = ecdsa.SECP256k1)
+        some_random_hash_string = hash_SHA("some_random_string".encode())
+        # Creating a signature that can only be created by the private key holder
+        signature = signing_key.sign(some_random_hash_string)
+        # Using the public key to varifying the signature
+        verifying_key = ecdsa.VerifyingKey.from_string(public_key, ecdsa.SECP256k1)
+        # Return if validity is true
+        self.assertTrue(verifying_key.verify(signature, some_random_hash_string))
+
+
 
 if __name__ == '__main__':
     unittest.main()
