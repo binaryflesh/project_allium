d0e715971bdf2909b990fc8797a0c1f4fa51e089
jldistefano <jldstefano@gmail.com>
2018-11-15 21:27:29 -0600

    Added extract() and associated tests

--- data_structures/blockchain.py
+++ data_structures/blockchain.py
@@ -14,3 +14,20 @@ class Blockchain:
         # If the file does not already exist, create the file and close it
         if not (os.path.isfile(filename)):
             with open(filename, 'wb') as f: pass
+
+def extract(filename, index, num_bytes, offset=0):
+    """
+    Opens file at filename, moves file pointer to correct position by using
+    adding index and offset, reads num_bytes bytes and returns them.
+    :param1 filename: String, path to a file containing bytes
+    :param2 index: Integer, representing where bytes should begin to read from
+    :param3 num_bytes: Integer, number of bytes to read in
+    :param4 offset: Integer, default 0. Number to adjust index by if file is offset  
+    :returns: Bytestring, representing bytes from index+offspring to index+offspring+num_bytes in filename
+    """
+    # Opens file for reading bytes
+    with open(filename, 'r+b') as file:
+        # Moves file pointer to correct position at index + offset
+        file.seek(index + offset, 0)
+        # Reads num_bytes after file pointer
+        return file.read(num_bytes)
--- data_structures/blockchain.test.py
+++ data_structures/blockchain.test.py
@@ -24,5 +24,30 @@ class TestBlock(unittest.TestCase):
 		    char = fileobj.read(1)
 		self.assertEqual(b'0', char)
 
+	def test_extract(self):
+		# Generates byte strings
+		bytes_1 = bytes("VADER: The Force is with you, Young Skywalker. But your not a Jedi yet. (BREATHING CONTINUES)", 'utf-8')
+		bytes_2 = bytes("Your destiny... lies with me, Skywalker. Obi-Wan knew is to be true.", 'utf-8')
+		bytes_3 = bytes("There is no escape. Don't make me destroy you. Luke. You do not yet realize... your importance. You've only become to... discover your power.", 'utf-8')
+		bytes_4 = bytes("Join me... and I will complete your training. With our combined strength... we can end this... destructive conflict... and bring order... to the galaxy.", 'utf-8')
+		# Writes byte strings to file
+		with open(self.bc.blockfile, 'w+b') as file:			
+			file.write(bytes_1)
+			file.write(bytes_2)
+			file.write(bytes_3)
+			file.write(bytes_4)
+		# Goal is to pick out third byte string
+		expected = bytes("There is no escape. Don't make me destroy you. Luke. You do not yet realize... your importance. You've only become to... discover your power.", 'utf-8')
+		# Index of first actual character from bytes_3, minus character name
+		index = len(bytes_1) + len(bytes_2) - 7
+		# Number of bytes to read is equal to length of bytes_3
+		num_bytes = len(bytes_3)
+		# 7 Byte offset, due to 7 unecessary bytes at beginning of file
+		offset = 7
+		# Attempts to extract bytes_3 from written file
+		actual = extract(self.bc.blockfile, index, num_bytes, offset)
+		# Tests manually written bytestring, and extracted bytestring
+		self.assertEqual(expected, actual)
+
 if __name__ == '__main__':
     unittest.main()
