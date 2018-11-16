c99e0806502456ffef4b7d0e07e031df33ec740c
Jacob Distefano <jdistefa@10-7-40-137.pclabs.uic.edu>
2018-11-16 17:04:20 -0600

    Updated file with current dev branch

--- data_structures/blockchain.py
+++ data_structures/blockchain.py
@@ -24,3 +24,20 @@ def get_size_bytes(byte_string):
     :returns: An integer in byte form
     """
     return pack('I', len(byte_string))
+
+def extract(filename, index, num_bytes, offset=0):
+  """
+  Opens file at filename, moves file pointer to correct position by using
+  adding index and offset, reads num_bytes bytes and returns them.
+  :param1 filename: String, path to a file containing bytes
+  :param2 index: Integer, representing where bytes should begin to read from
+  :param3 num_bytes: Integer, number of bytes to read in
+  :param4 offset: Integer, default 0. Number to adjust index by if file is offset  
+  :returns: Bytestring, representing bytes from index+offspring to index+offspring+num_bytes in filename
+  """
+  # Opens file for reading bytes
+  with open(filename, 'r+b') as file:
+      # Moves file pointer to correct position at index + offset
+      file.seek(index + offset, 0)
+      # Reads num_bytes after file pointer
+      return file.read(num_bytes)
