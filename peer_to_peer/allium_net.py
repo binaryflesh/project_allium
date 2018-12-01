74ac7fbb74d72405adb9d5a3f7678ae6994208ab
ZBASH <zlabas2@uic.edu>
2018-11-30 19:55:20 -0600

    Create a socket server

--- peer_to_peer/allium_net.py
+++ peer_to_peer/allium_net.py
@@ -11,3 +11,13 @@ def get_ip():
     ip = s.getsockname()[0]
     s.close()
     return ip
+
+def make_server(port=9001):
+    """return a socket object that includes a localhost IP and
+        and provided port number (or default of 9001)"""
+    server = socket.socket()
+    server.bind(('', port))
+
+    return server
+
+make_server()
