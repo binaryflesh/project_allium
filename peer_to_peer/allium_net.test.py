192adc8754a474ffe945fe0b14edc92dee9d63d3
karol <kstola2@uic.edu>
2018-12-01 19:18:52 -0600

    Need to close sockets to prevent resource warning

--- peer_to_peer/allium_net.test.py
+++ peer_to_peer/allium_net.test.py
@@ -10,22 +10,26 @@ class Test(unittest.TestCase):
 
     def tearDown(self): pass
 
+
     def test_ItCreatesAServer(self):
         """This test checks that a socket instance is createdIP"""
         server = make_server()
         self.assertIsInstance(server,socket.socket)
+        server.close()
 
     def test_ItReturnsTheExpectedIPAddress(self):
         """This test checks that a server created returns an expecgted IP address """
         ip = "0.0.0.0"
         server = make_server()
         self.assertEquals(ip,server.getsockname()[0])
+        server.close()
 
     def test_ItReturnsTheExpectedPortAddress(self):
         """This test checks that a server created returns an expectged port """
         port = 9001
         server = make_server()
         self.assertEqual(port,server.getsockname()[1])
+        server.close()
 
 if __name__ == '__main__':
     unittest.main()
