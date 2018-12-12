7a285db87cadba3721c450edd44538169ab074aa
karol <kstola2@uic.edu>
2018-12-12 01:38:51 -0600

    Added platform check and skipped a few networks.test.py tests

--- tests/master_test.py
+++ tests/master_test.py
@@ -1,6 +1,12 @@
 import sys
 import os
+import platform
+
+call = 'python3 '
+if 'Windows' in platform.platform():
+	call = 'python '
+	
 
 for filename in os.listdir(sys.path[0]):
 	if filename != "master_test.py" and filename.endswith(".py"):
-		os.system("python3 " + sys.path[0] + "/" + filename)
+		os.system(call + sys.path[0] + "/" + filename)
