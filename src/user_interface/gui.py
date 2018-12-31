a493acefce3fcf72ccc5e14346d9fd4cc4502a16
jldistefano <jldstefano@gmail.com>
2018-12-31 17:20:10 -0600

    Adds mine button

--- src/user_interface/gui.py
+++ src/user_interface/gui.py
@@ -20,16 +20,28 @@ class Gui(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setFixedSize(self.width, self.height)
+
+        #set up fonts to be used
+        self.bigFont = QFont("Arial", 16,QFont.Bold)
+        self.normalFont = QFont("Arial", 12)
+
+        # Sets the background image of the page to qbetdark.png
+        self.setBackgroundImage()
+        # Initializes the file bar
+        self.initFileBar()
+        # Initializes the mine button
+        self.initMineButton()
+
+        #set up a GridLayout
+        gridLayout = QGridLayout()
+        # Places the Mine Button in the top left corner of the screen
+        gridLayout.addWidget(self.mineButton, 0, 0)
+
         #create a QWidget and set it as a central widget
         #we need the QWidget because you cannot set a QLayout directly on QMainWindow
         centralWidget = QWidget(self)
         self.setCentralWidget(centralWidget)
-        #set up a GridLayout
-        gridLayout = QGridLayout()
         centralWidget.setLayout(gridLayout)
-        #Sets the background image of the page to qbetdark.png
-        self.setBackgroundImage()
-        self.initFileBar()
 
     def setBackgroundImage(self):
         # Creates a QImage Object with the image file
@@ -59,6 +71,18 @@ class Gui(QMainWindow):
 
         # TOOLS TAB =======================================================
         self.toolsMenu = self.mainMenu.addMenu('Settings')
+
+    def initMineButton(self):
+        self.mineButton = QPushButton("Mine", self)
+        self.mineButton.setFont(self.bigFont)
+        self.mineButton.setStyleSheet("""
+                            QWidget{
+                                background-color: rgb(20, 20, 20);
+                                color: rgb(200, 200, 200)
+                            }""")
+        #self.mineButton.clicked.connect("""MINE FUNCTION""")
+        self.mineButton.setFixedWidth(200)
+        self.mineButton.setFixedHeight(50)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
